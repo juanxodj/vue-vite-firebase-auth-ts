450dfca9fc340ba25c15bc05beb53f1a3fa4bdfe
Juan Sánchez <jlsc92@outlook.es>
2022-07-10 18:04:23 -0500

    fixing

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>About {{ userStore.userData }}</h1>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from "../stores/user";
+const userStore = useUserStore();
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Home {{ userStore.userData }}</h1>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from "../stores/user";
+const userStore = useUserStore();
+</script>
